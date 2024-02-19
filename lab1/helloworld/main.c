98080a156da76941138006c2e92eb9e0acc43e76
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-02-19 11:08:19 +0100

    hello world

---
+++ lab1/helloworld/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    printf("Hello world!\n");
+    int zmienna = 9;
+    scanf("%d", &zmienna);
+    printf("Zmienna to %d", zmienna);
+    return 0;
+}

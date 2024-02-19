2d32ef3f661d552d4e6a570cb2a0783c59d34830
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-02-19 11:14:03 +0100

    zad1

--- lab1/helloworld/main.c
+++ lab1/helloworld/main.c
@@ -5,7 +5,7 @@ int main()
 {
 
     printf("Hello world!\n");
-    int zmienna = 9;
+    int zmienna;
     scanf("%d", &zmienna);
     printf("Zmienna to %d", zmienna);
     return 0;
---
+++ lab1/zad1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int liczba;
+    scanf("%i", &liczba);
+    printf("%i", liczba);
+    return 0;
+}

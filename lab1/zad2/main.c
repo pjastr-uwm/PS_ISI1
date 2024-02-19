afe5a4c27f44990a6e0dc23c65e7dcd317827106
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-02-19 11:51:00 +0100

    zad

---
+++ lab1/zad2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float liczba1, liczba2;
+    scanf("%f", &liczba1),
+    scanf("%f", &liczba2);
+    printf("%f", liczba1-liczba2);
+    return 0;
+}

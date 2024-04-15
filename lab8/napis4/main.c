1adddf7c214f408be854748390c66b483b204738
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-04-15 11:56:13 +0200

    napis4

---
+++ lab8/napis4/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmpStrNew(char txt1[], char txt2[]){
+    int i;
+    for(i=0;txt1[i] != 0 && txt2[i] != 0;i++){
+        if (txt1[i] != txt2[i])
+            return 0;
+    }
+    if (txt1[i] != 0 || txt2[i] != 0)
+        return 0;
+    return 1;
+}
+
+int main()
+{
+    printf("%d\n", cmpStrNew("ABC", "ABCD"));
+    return 0;
+}

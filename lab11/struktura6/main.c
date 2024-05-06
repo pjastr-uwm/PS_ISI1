27bbedad2665b975b60caaa7b4a65d1f0fe2f7bf
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-05-06 11:54:48 +0200

    str 6

---
+++ lab11/struktura6/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Person{
+    char name[20];
+    int age;
+};
+
+struct Person * initPerson(char name2[], int age2){
+    struct Person * wsk = malloc(sizeof(struct Person));
+    //(*wsk).age = age2;
+    wsk->age = age2;
+    strcpy(wsk->name, name2);
+    return wsk;
+}
+
+void showPerson(struct Person arg){
+    printf("%s %d\n", arg.name, arg.age);
+}
+
+void birthday(struct Person * arg){
+    arg->age ++;
+}
+
+int main()
+{
+    struct Person * osoba = initPerson("Jan", 21);
+    showPerson(*osoba);
+    birthday(osoba);
+    showPerson(*osoba);
+    return 0;
+}

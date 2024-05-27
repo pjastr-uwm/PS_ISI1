9e8c471282894afba1d34ae7fc5bd8b89f15fbae
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2024-05-27 11:54:45 +0200

    zad 9 i 10

---
+++ lab13/zad10/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct element {
+    int x;
+    struct element * next;
+};
+
+void addFirst(struct element * lista, int a){
+    struct element * wsk = malloc(sizeof(struct element));
+    wsk->x = a;
+    wsk->next = lista->next;
+    lista->next = wsk;
+}
+
+void printListWithHead(struct element * lista){
+    struct element * wsk = lista->next;
+    while(wsk!=NULL){
+        printf("%d\n", wsk->x);
+        wsk = wsk->next;
+    }
+    printf("---\n");
+}
+
+
+int main()
+{
+    struct element * lista = malloc(sizeof(struct element));
+    lista->next = malloc(sizeof(struct element));
+    lista->next->x = 4;
+    lista->next->next = malloc(sizeof(struct element));
+    lista->next->next->x = 5;
+    lista->next->next->next = malloc(sizeof(struct element));
+    lista->next->next->next->x = -12;
+    lista->next->next->next->next = NULL;
+    printListWithHead(lista);
+    addFirst(lista, -30);
+    printListWithHead(lista);
+    return 0;
+}
